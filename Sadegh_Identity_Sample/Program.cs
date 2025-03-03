fb1d696aaeae58dc0d356e8f59d7853891b8da21
sadegh ashouri <sadeghashouri1374@gmail.com>
2025-03-03 18:17:46 +0330

    stage

--- Sadegh_Identity_Sample/Program.cs
+++ Sadegh_Identity_Sample/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Sadegh_Identity_Sample.Data;
+using Sadegh_Identity_Sample.Helper;
 using Sadegh_Identity_Sample.Models.Entities;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -10,7 +11,8 @@ string connectionString = configuration.GetConnectionString("DefaultConnection")
 // Add services to the container.
 builder.Services.AddIdentity<User, Role>()
     .AddEntityFrameworkStores<DataBaseContext>()
-    .AddDefaultTokenProviders();
+    .AddDefaultTokenProviders()
+    .AddErrorDescriber<CustomIdentityError>();
 
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<DataBaseContext>(
